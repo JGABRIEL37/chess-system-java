53e7ef9c9303917606168ca8e6ef98a622b36c69
JGABRIEL37 <jbiel72001@gmail.com>
2020-01-04 15:46:31 -0300

    Placing pieces on the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package application;
 
 import boardgame.Board;
 import chess.ChessMatch;
 
-
-/**
- *
- * @author Cliente
- */
 public class Program {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-   
+
         ChessMatch chessMatch = new ChessMatch();
         UI.printBoard(chessMatch.getPieces());
     }
-    
+
 }
